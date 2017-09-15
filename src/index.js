847145a9f24824116e6c7c1488038fbe991219b1
Liudmila Pi <milalindley@gmail.com>
2017-09-15 18:21:38 +0300

    brackets/feature: brackets sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+var openBrackets = [];
+var closeBrackets = [];
+var stack = [];
+
+var column = (arr, n) => arr.map(x => x[n]);
+
+function isOpen(br){
+    if ((openBrackets.indexOf(br) != -1) && (closeBrackets.indexOf(br) != -1)){
+        let i = stack.length-1;
+        return (stack[i] != br) || (stack[i-1] == stack[i]);
+    } else {
+        return openBrackets.indexOf(br) != -1;
+    }
 }
+
+function isClose(br){
+    return closeBrackets.indexOf(br) != -1;
+}
+
+function matching(openBr, closeBr){
+    return openBrackets.indexOf(openBr) == closeBrackets.indexOf(closeBr);
+}
+
+function stackIsEmpty(arr){
+    return ((arr == undefined) || (arr.length == 0));
+}
+
+module.exports = function check(str, bracketsConfig) {
+    stack = [];
+    openBrackets = column(bracketsConfig,0);
+    closeBrackets = column(bracketsConfig,1);
+    let br;
+
+    for (i = 0; i < str.length; i++){
+        br = str[i];
+        if (isOpen(br)) {
+            stack.push(br);
+        } else if (isClose(br)){
+            if (stackIsEmpty(stack) || !matching(stack.pop(),br)){
+                return false;
+            }
+        }
+    }
+    return stackIsEmpty(stack);
+};
